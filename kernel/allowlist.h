5470d7bde5c8b077e715b986cb5b517b36427783
blu290 <>
2024-12-01 12:06:12 +0000

    test

--- kernel/allowlist.h
+++ kernel/allowlist.h
@@ -23,5 +23,6 @@ bool ksu_get_app_profile(struct app_profile *);
 bool ksu_set_app_profile(struct app_profile *, bool persist);
 
 bool ksu_uid_should_umount(uid_t uid);
+
 struct root_profile *ksu_get_root_profile(uid_t uid);
 #endif
